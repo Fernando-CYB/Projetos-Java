28fa85d6fe35cbe5b3a1bb924fe267c311dba770
Fernando <FernandoJoseSantosLima@outlook.com>
2026-01-10 22:08:58 -0300

    Add Calculator project (Java OOP)

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,39 @@
+public class Calculator {
+
+  public Calculator() {}
+
+  public int add(int a, int b){
+    return a + b;
+  }
+
+  public int subtract(int a, int b){
+    return a - b;
+  }
+
+  public int multiply(int a, int b){
+    return a * b;
+  }
+
+  public int divide(int a, int b){
+    if(b == 0){
+      throw new ArithmeticException("Cannot divide by zero");
+    }
+    return a / b; 
+  }
+
+  public int modulo(int a, int b){
+    if(b == 0){
+      throw new ArithmeticException("Cannot modulo by zero");
+    }
+    return a % b;
+  }
+
+  public static void main(String[] args){
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5,7));
+    System.out.println(myCalculator.subtract(45,11));
+    System.out.println(myCalculator.multiply(35,4));
+    System.out.println(myCalculator.divide(600,15));
+    System.out.println(myCalculator.modulo(255,122));
+  }
+}
